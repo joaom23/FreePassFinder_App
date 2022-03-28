ce01b740a9e28ba8b82a4a38a4f952e45795819d
joaom23 <82104896+joaom23@users.noreply.github.com>
2022-03-28 17:32:17 +0100

    Update IP_API.cs

--- AppFP/AppFP/AppFP/Helpers/IP_API.cs
+++ AppFP/AppFP/AppFP/Helpers/IP_API.cs
@@ -6,7 +6,7 @@ namespace AppFP.Helpers
 {
     public static class IP_API
     {
-        public static readonly string APIserver = "http://89.114.83.11:5000";
+        public static readonly string APIserver = "http://0.0.0.0:5000";
         //public static readonly string APIserver = "http://192.168.1.72:5000";
         //public static readonly string APIserver = "http://10.0.2.2:41597";
     }
